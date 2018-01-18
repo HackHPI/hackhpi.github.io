9f6e3a2f462d25e3cc86ecfd6aabb61df3b0ab0b
Jakob Edding <jak-ing@users.noreply.github.com>
2018-01-18 12:36:08 +0100

    Save live page to repo for later use

---
+++ disabled_live_page/live_files/app.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+
+	$("#countdown").countdown("06/11/2016 10:00:00", function(event) {
+		$(this).text(
+			event.strftime('%D:%H:%M:%S')
+		);
+	});
+
+
+	var $mainNavLinks = $('#main-nav a');
+
+    $('a[href^="/#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        if($(e.target).closest('#main-nav').length) {
+	        $mainNavLinks.each(function () {
+	            $(this).removeClass('active');
+	        })
+	        $(this).addClass('active');
+	    }
+      
+        var target = this.hash,
+            menu   = target;
+
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+    var running = 0;
+	function onScroll(event){
+	    var scrollPos = $(document).scrollTop();
+	    var headerHeight = $('header').height();
+
+	    var $mainNav = $('#main-nav');
+	    var mainNavHeight = $mainNav.height();
+	    var $mainNavLinks = $('#main-nav a');
+
+	    if(scrollPos > headerHeight) {
+	    	if(!$mainNav.hasClass('fixed')) {
+		    	$mainNav.addClass('fixed').css('top', (-1) * mainNavHeight);
+				$mainNav.stop().animate({
+					'top':0
+				}, 150, 'swing');
+			}
+	    } else if($mainNav.hasClass('fixed')) {
+	    	if(running != 1) {
+	    		running = 1;
+		    	$mainNav.stop().animate({
+					'top':(-1) * mainNavHeight
+				}, 150, 'swing', function() {
+					$mainNav.removeClass('fixed').css('top', 50);
+					running = 0;
+				});
+			}
+	    }
+ 		
+
+	    $mainNavLinks.each(function () {
+	        var currLink = $(this);
+	        var refElement = $('#' + currLink.data("target"));
+
+	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	            $mainNavLinks.removeClass("active");
+	            currLink.addClass("active");
+	        } else {
+	            currLink.removeClass("active");
+	        }
+	    });
+	}
+
+	$(document).on("scroll", onScroll);
+
+});

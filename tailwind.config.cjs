0de007984e2470d391c9c93261f750d44027d75e
Benjamin Frost <bfrost.schule@gmail.com>
2022-11-06 17:27:51 +0100

    feat: use hpi colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        hpi: {
+          blue: '#007a9e',
+          gray: '#5a6065',
+          orange: '#dd6108',
+          red: '#b1063a',
+          yellow: '#f6a800',
+        },
+      },
+    },
+  },
+  plugins: [],
+};

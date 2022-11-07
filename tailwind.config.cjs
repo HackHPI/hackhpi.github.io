1f0d1cc2c396c55f4e86d52b86aa783a9408ea6d
Benjamin Frost <bfrost.schule@gmail.com>
2022-11-07 20:07:35 +0100

    feat: improve hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      backgroundImage: {
+        hero: "url('/hero.jpg')",
+      },
       colors: {
         hpi: {
           blue: '#007a9e',

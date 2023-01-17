50d7e50529f9437ccfc6447b16d46cf2b993d4d1
Benjamin Frost <bfrost.schule@gmail.com>
2023-01-17 19:20:32 +0100

    ci: add deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://hackhpi.org',
 });

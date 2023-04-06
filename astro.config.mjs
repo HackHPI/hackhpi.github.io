52e5d9c497944eb5a6809da561bc26f40b5848a5
Benjamin Frost <bfrost.schule@gmail.com>
2023-04-06 17:16:09 +0200

    feat: add astro react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://hackhpi.org',
+  integrations: [tailwind(), react()],
+  site: 'https://hackhpi.org'
 });

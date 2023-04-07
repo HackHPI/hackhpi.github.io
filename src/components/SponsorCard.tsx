acf48b3a3b95991570ae4c1e62413ec2d3c4b28c
Benjamin Frost <bfrost.schule@gmail.com>
2023-04-07 13:58:27 +0200

    fix: sponsors ui on mobile

--- src/components/SponsorCard.tsx
+++ src/components/SponsorCard.tsx
@@ -51,7 +51,7 @@ export default function SponsorCard({ name, image, description, url }: Props) {
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"
@@ -61,8 +61,8 @@ export default function SponsorCard({ name, image, description, url }: Props) {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl sm:p-6">
-                  <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-3xl sm:p-6">
+                  <div className="absolute right-0 top-0 pr-4 pt-4">
                     <button
                       type="button"
                       className="rounded-md bg-white text-gray-400 hover:text-gray-500 "

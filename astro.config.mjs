ecb924e3edc2087233dd9cf39dbc4d6658040713
Benjamin Frost <bfrost.schule@gmail.com>
2023-04-22 12:48:30 +0200

    feat: image gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://hackhpi.org'
+  site: 'https://hackhpi.org/',
+  experimental: {
+    assets: true,
+  },
 });

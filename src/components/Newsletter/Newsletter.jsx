3d870d8d7e0f723a503796b1f4ff01587c3e3add
Cedric Rische <cedric.rische@cedsoft.de>
2024-02-08 18:46:01 +0100

    Fixed FAQ typo Added dark newsletter iframe Removed Team position

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -10,12 +10,10 @@ function Newsletter() {
                         <iframe
                             width="540"
                             height="350"
-                            src="https://0270cddf.sibforms.com/serve/MUIFABVV0kHidS5IzNgtqUvG1rLtlftSX4W4uGTyVdinFJUl058WKYsaVKDfSCnHUEM0gpwj7NC_as6DZ0aiy3J1wPqQg9XgIuVMzy5Q30AsiVpWDZpINCrvwq8kam_Q7zLV9zvBwixIk5NPCKR4416I7E_DBsT42vuRyN5DGcp6qRZQIvs8PijiNYwdZ7AT2JuafhM1Xbr8dBHs"
-                            frameBorder="0"
-                            scrolling="auto"
-                            allowFullScreen
-                            //style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"
+                            src="https://0270cddf.sibforms.com/serve/MUIFAODxMCBfEOxAaU41kdXNneuAYA9UFi6EdbUH-mezqAUhIK5cEXk8oEIPnRLFxdhcbYk-LxaUNXsHG-wKikEJ-7PxUY0h8Fyfba1LtUBOhpVSwUWzjyx4QY0n2N5vM52i5u1HwqcZrC-rJaAxAIndH_nvEadZlRyjy3Ky_NncN66lIPLtXLFN5JWjZNGqaNxCAV0bz1CE_EbY"
+                            frameBorder="0" scrolling="auto" allowFullScreen
                         ></iframe>
+
                     </Grid>
                     <Grid item xs={12} md={6}>
                         <Typography

69fd3b29e26ddbf1d565d95bfdc251af6a750e03
Cedric Rische <cedric.rische@cedsoft.de>
2024-02-10 13:58:13 +0100

    Bug fixes

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,10 +1,11 @@
 import {Container, Typography} from "@mui/material";
 
 function Registration() {
-    return(
+    return (
         <Container maxWidth={"xl"} sx={{paddingTop: 10, paddingBottom: 10}}>
             <Typography variant={"h1"}>Registration</Typography>
         </Container>
     )
 }
+
 export default Registration

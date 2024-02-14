b6af735d56444f4795e487c5d247d94bd3851672
Cedric Rische <cedric.rische@cedsoft.de>
2024-02-14 21:11:16 +0100

    Some changes

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -2,8 +2,8 @@ import {Container, Typography} from "@mui/material";
 
 function Registration() {
     return (
-        <Container maxWidth={"xl"} sx={{paddingTop: 10, paddingBottom: 10}}>
-            <Typography variant={"h1"}>Registration</Typography>
+        <Container sx={{paddingTop: 10, paddingBottom: 10}}>
+            <Typography variant={"h2"} component={"h1"}>Registration</Typography>
         </Container>
     )
 }

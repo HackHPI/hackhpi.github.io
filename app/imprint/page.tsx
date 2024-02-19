117d5ed1322e9720ccdb1f1b8f12e9b2e60fb620
Cedric Rische <cedric.rische@cedsoft.de>
2024-02-19 18:21:15 +0100

    Deleted and rearranged some files Deleted Astro Deleted Tailwind Deleted old team files Deleted Astro files

--- app/imprint/page.tsx
+++
@@ -1,6 +0,0 @@
-import Imprint from "../../src/components/Imprint/Imprint";
-import HackHPIWrapper from "../../src/components/Theme/HackHPIWrapper";
-
-export default function Page(){
-    return <HackHPIWrapper><Imprint/></HackHPIWrapper>
-}

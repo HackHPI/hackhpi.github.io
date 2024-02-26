a39d87db73e3f88fe9b6654fc999b1b307547afc
Cedric Rische <cedric.rische@cedsoft.de>
2024-02-26 17:40:37 +0100

    Added subtext of Bitcom

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,14 +1,13 @@
 "use client"
-import {Box, Container, Grid, Link, Paper, Stack, Typography, useTheme} from "@mui/material";
+import {Box, Container, Grid, Paper, Stack, Typography, useTheme} from "@mui/material";
 import FelixLennardHake from "../../assets/images/speakers/FelixLennardHake.jpg"
 import {WindowCard} from "../WindowCard/WindowCard";
-import {LinkedIn} from "@mui/icons-material";
 
 const speakers = {
     primary: [
         {
             name: "Felix Lennard Hake",
-            position: "Mobility policy officer at Bitkom",
+            position: "Mobility policy officer at Bitkom, Germany's digital association",
             profilePicture: FelixLennardHake.src,
             description: "Felix is responsible for Bitkom's political and regulatory work on smart mobility. Together with manufacturers, tech companies, mobility service providers and transport companies, Felix is committed to a connected, intelligent and sustainable mobility ecosystem - in rural areas as well as in cities and metropolitan regions. He is also responsible for the Digital Aviation working group. Prior to joining Bitkom, Felix worked for a political strategy and communications consultancy for several years and completed a Blue Book traineeship in the EU Commission's DG CONNECT. He studied sociology, politics and economics as well as European studies and political science in Friedrichshafen, Paris and Berlin.",
             linkedIn: "felix-lennart-hake"

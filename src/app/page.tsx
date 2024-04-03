9d412e18026ae957e7f884d2275bfa3991bbdeb3
tanja04 <tanjal04@outlook.de>
2024-04-03 17:37:08 +0200

    Added Jury

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import {Gallery} from "../components/Gallery/Gallery";
 import {Faq} from "../components/Faq/Faq";
 import Team from "../components/Team/Team";
 import Speakers from "../components/Speakers/Speakers";
+import Jury from "../components/Jury/Jury";
 import {Features} from "../components/Features/Features";
 import LandingImage from "../components/LandingImage/LandingImage";
 import {Location} from "../components/Location/Location";
@@ -23,6 +24,7 @@ export default function Page() {
             <Registration/>
             <Location/>
             <Speakers/>
+            <Jury/>
             <TimeTable />
             <Gallery />
             <Faq />

cd19a62bb5806c70e406b33683d30c87b41f6bfb
Cedric Rische <cedric.rische@cedsoft.de>
2024-04-21 23:27:57 +0200

    Added new images. Restructuring of sections

---
+++ src/assets/images/event/2023/Images2023.js
@@ -0,0 +1,113 @@
+
+import DSC02572 from './DSC02572_1920.webp'
+import DSC02594 from './DSC02594_1920.webp'
+import DSC02601 from './DSC02601_1920.webp'
+import DSC02604 from './DSC02604_1920.webp'
+import DSC02609 from './DSC02609_1920.webp'
+import DSC02617 from './DSC02617_1920.webp'
+import DSC02619 from './DSC02619_1920.webp'
+import DSC02630 from './DSC02630_1920.webp'
+import DSC02632 from './DSC02632_1920.webp'
+import DSC02635 from './DSC02635_1920.webp'
+import DSC02638 from './DSC02638_1920.webp'
+import DSC02639 from './DSC02639_1920.webp'
+import DSC02643 from './DSC02643_1920.webp'
+import DSC02645 from './DSC02645_1920.webp'
+import DSC02646 from './DSC02646_1920.webp'
+import DSC02659 from './DSC02659_1920.webp'
+import DSC02677 from './DSC02677_1920.webp'
+import DSC02680 from './DSC02680_1920.webp'
+import DSC02762 from './DSC02762_1920.webp'
+import DSC02810 from './DSC02810_1920.webp'
+import DSC02820 from './DSC02820_1920.webp'
+import DSC02823 from './DSC02823_1920.webp'
+import DSC02824 from './DSC02824_1920.webp'
+import DSC02830 from './DSC02830_1920.webp'
+import DSC02868 from './DSC02868_1920.webp'
+import DSC02870 from './DSC02870_1920.webp'
+import DSC02872 from './DSC02872_1920.webp'
+import DSC02876 from './DSC02876_1920.webp'
+import DSC02879 from './DSC02879_1920.webp'
+import DSC02886 from './DSC02886_1920.webp'
+import DSC02889 from './DSC02889_1920.webp'
+import DSC02903 from './DSC02903_1920.webp'
+import DSC02908 from './DSC02908_1920.webp'
+import DSC02914 from './DSC02914_1920.webp'
+import DSC02919 from './DSC02919_1920.webp'
+import DSC02920 from './DSC02920_1920.webp'
+import DSC02923 from './DSC02923_1920.webp'
+import DSC02926 from './DSC02926_1920.webp'
+import DSC02928 from './DSC02928_1920.webp'
+import DSC02931 from './DSC02931_1920.webp'
+import DSC02941 from './DSC02941_1920.webp'
+import DSC02946 from './DSC02946_1920.webp'
+import DSC02949 from './DSC02949_1920.webp'
+import DSC02957 from './DSC02957_1920.webp'
+import DSC02962 from './DSC02962_1920.webp'
+import DSC02969 from './DSC02969_1920.webp'
+import DSC02974 from './DSC02974_1920.webp'
+import DSC02983 from './DSC02983_1920.webp'
+import DSC02992 from './DSC02992_1920.webp'
+import DSC02998 from './DSC02998_1920.webp'
+import DSC03006 from './DSC03006_1920.webp'
+import DSC03022 from './DSC03022_1920.webp'
+import DSC03032 from './DSC03032_1920.webp'
+
+const Images2023 = [
+    DSC02572,
+    DSC02594,
+    DSC02601,
+    DSC02604,
+    DSC02609,
+    DSC02617,
+    DSC02619,
+    DSC02630,
+    DSC02632,
+    DSC02635,
+    DSC02638,
+    DSC02639,
+    DSC02643,
+    DSC02645,
+    DSC02646,
+    DSC02659,
+    DSC02677,
+    DSC02680,
+    DSC02762,
+    DSC02810,
+    DSC02820,
+    DSC02823,
+    DSC02824,
+    DSC02830,
+    DSC02868,
+    DSC02870,
+    DSC02872,
+    DSC02876,
+    DSC02879,
+    DSC02886,
+    DSC02889,
+    DSC02903,
+    DSC02908,
+    DSC02914,
+    DSC02919,
+    DSC02920,
+    DSC02923,
+    DSC02926,
+    DSC02928,
+    DSC02931,
+    DSC02941,
+    DSC02946,
+    DSC02949,
+    DSC02957,
+    DSC02962,
+    DSC02969,
+    DSC02974,
+    DSC02983,
+    DSC02992,
+    DSC02998,
+    DSC03006,
+    DSC03022,
+    DSC03032,
+
+]
+
+export default Images2023

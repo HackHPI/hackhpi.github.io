c4544359f1c3e893d2de979b980bbb5141a7f015
tanja04 <tanjal04@outlook.de>
2025-08-04 13:04:03 +0200

    Added sponsors26

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import {VerifiedChecker} from "../components/VerifiedChecker/VerifiedChecker";
 import WhatToExpect from "../components/WhatToExpect/WhatToExpect";
-import Sponsors25 from "../components/Sponsors/Sponsors25";
+import Sponsors26 from "../components/Sponsors/Sponsors26";
 import Newsletter from "../components/Newsletter/Newsletter";
 import {Faq} from "../components/Faq/Faq";
 import Team from "../components/Team/Team";
@@ -19,7 +19,7 @@ export default function Page() {
             <VerifiedChecker/>
             <WhatToExpect/>
             <Features/>
-            <Sponsors25/>
+            <Sponsors26/>
             <Gallery/>
             <Newsletter/>
             {/* <Registration /> */}
